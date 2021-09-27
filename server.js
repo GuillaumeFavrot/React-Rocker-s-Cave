4e8886d83686d5f1133adc0e3e1dbab7f6169eb4
Guthan-FR <gui.favrot@gmail.com>
2021-09-27 19:26:35 +0200

    server.js corrections

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ if(process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'))
     
-    app.get('*', (req, re) => {
-        ress.sendFile(pathh.resolve(__dirname, 'client', 'buuild', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
